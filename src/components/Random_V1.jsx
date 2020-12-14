c82ee20563bb70ddc0eb28a50d80174a4cc8d7f4
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-12-15 00:44:40 +0530

    commit: api key in .env

---
+++ src/components/Random_V1.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React, { useState, useEffect } from "react";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const Random = () => {
+  return (
+    <div>
+      <h1>Random</h1>
+    </div>
+  );
+};
+export default Random;
